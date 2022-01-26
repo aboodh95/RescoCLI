dfa9dc60906e011608efa8104714244f678eeaf4
Abdulmounem Alhamwi <abod.h95@gmail.com>
2022-01-26 11:22:50 +0300

    Added support for Update All Projects by one call

--- RescoCLI/Tasks/Offline-html/OfflineHTMLUpdaterCmd.cs
+++ RescoCLI/Tasks/Offline-html/OfflineHTMLUpdaterCmd.cs
@@ -21,6 +21,10 @@ namespace RescoCLI.Tasks
 
         [Option(CommandOptionType.NoValue, ShortName = "d", LongName = "isDevelopment", Description = "Pushing To Development", ValueName = "isDevelopment", ShowInHelpText = true)]
         public bool isDevelopment { get; set; } = true;
+
+        [Option(CommandOptionType.NoValue, ShortName = "a", LongName = "All", Description = "Update All Projects", ValueName = "All", ShowInHelpText = true)]
+        public bool UpdateAll { get; set; } = false;
+     
         [Option(CommandOptionType.SingleValue, ShortName = "i", LongName = "ProjectIndex", Description = "The index of project configuration", ValueName = "ProjectIndex", ShowInHelpText = true)]
         public int projectIndex { get; set; }
 
@@ -38,18 +42,34 @@ namespace RescoCLI.Tasks
         protected override async Task<int> OnExecute(CommandLineApplication app)
         {
             var configuration = await Configuration.GetConfigrationAsync();
-            OfflineHTMLConfiguration offlineHTMLConfiguration = configuration.OfflineHTMLConfigurations.ElementAtOrDefault(projectIndex);
-            if (offlineHTMLConfiguration == null)
+            Dictionary<string, string> FolderNameAndPath = new Dictionary<string, string>();
+            if (UpdateAll)
             {
-                throw new Exception($"Configuration with index: {projectIndex} doesn't exists");
+                var configurationByProject = configuration.OfflineHTMLConfigurations.GroupBy(x => x.SelectedProjectId);
+                foreach (var item in configurationByProject)
+                {
+                    FolderNameAndPath = item.ToDictionary(x => x.FolderName, x => x.FolderPath);
+                    var selectedConnections = configuration.Connections.FirstOrDefault(x => x.IsSelected);
+                    await PushFiles(selectedConnections.URL, new NetworkCredential(selectedConnections.UserName, selectedConnections.Password), item.Key, FolderNameAndPath, isDevelopment);
+                }
+             
             }
-
-            var selectedConnections = configuration.Connections.FirstOrDefault(x => x.IsSelected);
-            await PushFiles(selectedConnections.URL, new NetworkCredential(selectedConnections.UserName, selectedConnections.Password), offlineHTMLConfiguration.SelectedProjectId, offlineHTMLConfiguration.FolderPath, offlineHTMLConfiguration.FolderName, isDevelopment);
+            else
+            {
+                OfflineHTMLConfiguration offlineHTMLConfiguration = configuration.OfflineHTMLConfigurations.ElementAtOrDefault(projectIndex);
+                if (offlineHTMLConfiguration == null)
+                {
+                    throw new Exception($"Configuration with index: {projectIndex} doesn't exists");
+                }
+                FolderNameAndPath.Add(offlineHTMLConfiguration.FolderName, offlineHTMLConfiguration.FolderPath);
+                var selectedConnections = configuration.Connections.FirstOrDefault(x => x.IsSelected);
+                await PushFiles(selectedConnections.URL, new NetworkCredential(selectedConnections.UserName, selectedConnections.Password), offlineHTMLConfiguration.SelectedProjectId, FolderNameAndPath, isDevelopment);
+            }
+         
             return 0;
         }
 
-        public async Task PushFiles(string url, NetworkCredential networkCredential, string projectId, string folderPath, string folderName, bool isDevelopment)
+        public async Task PushFiles(string url, NetworkCredential networkCredential, string projectId,Dictionary<string,string> FolderNameAndPath , bool isDevelopment)
         {
             Spinner spinner = new Spinner();
             spinner.Start();
@@ -60,18 +80,21 @@ namespace RescoCLI.Tasks
             Console.WriteLine("Exporting Project...");
             var zipFilePath = await dataService.ExportProjectAsync(projectId);
             var zipFile = ZipFile.Open(zipFilePath, ZipArchiveMode.Update);
-
             var zipFolderPath = zipFilePath.Replace(".zip", "");
-            var distPath = Path.Combine(zipFolderPath, "www", folderName);
             zipFile.ExtractToDirectory(zipFolderPath);
-
-            if (Directory.Exists(distPath))
+            foreach (var item in FolderNameAndPath)
             {
-                Directory.Delete(distPath, true);
+                var distPath = Path.Combine(zipFolderPath, "www", item.Key);
+                if (Directory.Exists(distPath))
+                {
+                    Directory.Delete(distPath, true);
+                }
+                Directory.CreateDirectory(distPath);
+                Console.WriteLine("Updating Files...");
+                CopyFoldersAndFiles(item.Value, distPath,isDevelopment);
             }
-            Directory.CreateDirectory(distPath);
-            Console.WriteLine("Updating Fiels...");
-            CopyFoldersAndFiles(folderPath, distPath);
+           
+
             var newZipPath = $"{Path.GetTempPath()}\\{Guid.NewGuid()}.zip";
             ZipFile.CreateFromDirectory(zipFolderPath, newZipPath);
             Console.WriteLine("Importing Project...");
@@ -79,13 +102,30 @@ namespace RescoCLI.Tasks
             spinner.Stop();
         }
 
-        private void CopyFoldersAndFiles(string folderPath, string distPath)
+        private void CopyFoldersAndFiles(string folderPath, string distPath,bool isDevelopment)
         {
             var files = Directory.GetFiles(folderPath);
             var folders = Directory.GetDirectories(folderPath);
             foreach (var item in files)
             {
                 FileInfo fileInfo = new(item);
+                if (fileInfo.Extension.ToLower() == ".js")
+                {
+                    if (isDevelopment)
+                    {
+                        if (item.EndsWith(".min.js"))
+                        {
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        if (!item.EndsWith(".min.js"))
+                        {
+                            continue;
+                        }
+                    }
+                }
                 File.Copy(item, Path.Combine(distPath, fileInfo.Name));
             }
             foreach (var item in folders)
@@ -96,7 +136,7 @@ namespace RescoCLI.Tasks
                 {
                     Directory.CreateDirectory(newFolderPath);
                 }
-                CopyFoldersAndFiles(item, newFolderPath);
+                CopyFoldersAndFiles(item, newFolderPath,isDevelopment);
             }
         }
     }
