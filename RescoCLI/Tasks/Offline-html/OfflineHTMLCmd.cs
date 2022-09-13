e237c785a25ce7a6257c571a15b0d669dc36d658
Abood Alhamwi <abod.h95@gmail.com>
2022-09-13 08:58:36 +0300

    Added Project Name to the Control

--- RescoCLI/Tasks/Offline-html/OfflineHTMLCmd.cs
+++ RescoCLI/Tasks/Offline-html/OfflineHTMLCmd.cs
@@ -20,6 +20,9 @@ namespace RescoCLI.Tasks
     [Subcommand(typeof(OfflineHTMLUpdaterCmd))]
     class OfflineHTMLCmd : RescoCLIBase
     {
+
+        Resco.Cloud.Client.WebService.DataService _service;
+
         public OfflineHTMLCmd(ILogger<RescoCLICmd> logger, IConsole console)
         {
 
@@ -33,15 +36,29 @@ namespace RescoCLI.Tasks
             {
                 throw new Exception("Default Project is not selected");
             }
-
+            _service = new Resco.Cloud.Client.WebService.DataService(selectedConnections.URL)
+            {
+                Credentials = new NetworkCredential(selectedConnections.UserName, selectedConnections.Password)
+            };
         }
 
         protected override async Task<int> OnExecute(CommandLineApplication app)
         {
             var configuration = await Configuration.GetConfigrationAsync();
+            var folder = System.AppDomain.CurrentDomain.BaseDirectory;
+            var fetch = new Fetch("mobileproject");
+            fetch.Entity.AddAttribute("name");
+            fetch.Entity.AddAttribute("id");
+            fetch.Entity.AddAttribute("resco_appid");
+            fetch.Entity.AddAttribute("resco_parents");
+            fetch.Entity.OrderBy("createdon", false);
+
+            var projects = _service.Fetch(fetch).Entities;
+
             for (int i = 0; i < configuration.OfflineHTMLConfigurations.Count; i++)
             {
-                Console.WriteLine($"{i} - {configuration.OfflineHTMLConfigurations[i].FolderName}");
+                var project = projects.Find(x => x["id"].ToString() == configuration.OfflineHTMLConfigurations[i].SelectedProjectId);
+                Console.WriteLine($"{i} - {configuration.OfflineHTMLConfigurations[i].FolderName} - {project?["name"]}");
             }
 
             return await Task.FromResult(0);
